fe5ee889a39fd00824409befe435ab1a62877a0c
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-06-24 11:43:16 +0200

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeaturedSection from '../components/FeaturedSection'
+import TrailersSection from '../components/TrailersSection'
 
 const Home = () => {
   return (
     <div>
-      
+      <HeroSection/>
+      <FeaturedSection/>
+      <TrailersSection/>
     </div>
   )
 }
